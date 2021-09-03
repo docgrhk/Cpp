751dc9c3954a31215af60884c178747718dc5790
docgrhk <84428358+docgrhk@users.noreply.github.com>
2021-09-03 10:21:25 +0800

    AMA2222

---
+++ AMA2222/Lec1/Variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,y;
+	x=4;
+	y=3;
+	cout << x/y << endl;
+	return 0;
+}
---
+++ AMA2222/Lec1/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number1, number2, sum;
+	cout << "Enter the first integer";
+	cin >> number1;
+	cout << "Enter the second integer";
+	cin >> number2;
+	
+}
